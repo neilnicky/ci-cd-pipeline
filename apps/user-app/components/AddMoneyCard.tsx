2121eacbaf6728c1264c7c2644c7ec4c6e1e60d6
neilnicky <neilnicholaskanat@gmail.com>
2025-01-19 19:59:04 +0530

    test: github workflows

--- apps/user-app/components/AddMoneyCard.tsx
+++ apps/user-app/components/AddMoneyCard.tsx
@@ -40,7 +40,7 @@ export const AddMoney = () => {
                 await createOnRampTransaction(amount * 100, provider)
                 window.location.href = redirectUrl || "";
             }}>
-            Add Money
+            Add Moneyy
             </Button>
         </div>
     </div>
